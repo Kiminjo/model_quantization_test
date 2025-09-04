1b6f26e1e4848d194c9f51cf39667a9d3626bd65
Kiminjo <deulseong1121@gmail.com>
2025-09-04 23:17:48 +0900

    chore: 스크립트 파일 수정

---
+++ run_compare.sh
@@ -0,0 +1,15 @@
+python compare_data_type.py \
+    --fp32-engine runs/detect/baseline_yolo11n_imgsz400_20250901_234229/exported_models/FP32/best.engine \
+    --fp16-engine runs/detect/baseline_yolo11n_imgsz400_20250901_234229/exported_models/FP16/best.engine \
+    --ptq-engine runs/detect/baseline_yolo11n_imgsz400_20250901_234229/exported_models/INT8_PTQ/best.engine \
+    --qat-engine runs/qat/qat_best_20250902_074445/best.engine \
+    --data data/processed/object_detection/data.yaml \
+    --device cuda 
+
+
+python compare_platform_speed.py \
+    --pt-model runs/qat/qat_best_20250902_074445/best.pt \
+    --onnx-model runs/qat/qat_best_20250902_074445/best.onnx \
+    --trt-model runs/qat/qat_best_20250902_074445/best.engine \
+    --device cuda \
+    --save-plot results/speed_comparison_by_platform.png
