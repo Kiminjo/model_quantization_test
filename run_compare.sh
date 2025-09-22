52378a31ab0cfbd894c7ba17577d449d759bad46
Kiminjo <deulseong1121@gmail.com>
2025-09-22 10:30:02 +0900

    chore: 테스트 실행 스크립트 args 값 변경

--- run_compare.sh
+++ run_compare.sh
@@ -1,15 +1,15 @@
 python compare_data_type.py \
-    --fp32-engine runs/detect/baseline_yolo11n_imgsz400_20250901_234229/exported_models/FP32/best.engine \
-    --fp16-engine runs/detect/baseline_yolo11n_imgsz400_20250901_234229/exported_models/FP16/best.engine \
-    --ptq-engine runs/detect/baseline_yolo11n_imgsz400_20250901_234229/exported_models/INT8_PTQ/best.engine \
-    --qat-engine runs/qat/qat_best_20250902_074445/best.engine \
+    --fp32-engine runs/detect/baseline_yolo11n_imgsz400_20250917_221905/exported_models/FP32/best.engine \
+    --fp16-engine runs/detect/baseline_yolo11n_imgsz400_20250917_221905/exported_models/FP16/best.engine \
+    --ptq-engine runs/detect/baseline_yolo11n_imgsz400_20250917_221905/exported_models/INT8_PTQ/best.engine \
+    --qat-engine runs/qat/qat_best_20250917_224550/best.engine \
     --data data/processed/object_detection/data.yaml \
     --device cuda 
 
 
 python compare_platform_speed.py \
-    --pt-model runs/qat/qat_best_20250902_074445/best.pt \
-    --onnx-model runs/qat/qat_best_20250902_074445/best.onnx \
-    --trt-model runs/qat/qat_best_20250902_074445/best.engine \
+    --pt-model runs/qat/qat_best_20250917_224550/best.pt \
+    --onnx-model runs/qat/qat_best_20250917_224550/best.onnx \
+    --trt-model runs/qat/qat_best_20250917_224550/best.engine \
     --device cuda \
     --save-plot results/speed_comparison_by_platform.png
